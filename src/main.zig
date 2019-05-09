097775dab6492b403d4b53ada9f0de56a582340d
Luna <git@l4.pm>
2019-05-09 17:51:49 -0300

    add arg fetch

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
+const os = std.os;
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your base are belong to us.\n");
+    var da = std.heap.DirectAllocator.init();
+    var arena = std.heap.ArenaAllocator.init(&da.allocator);
+    errdefer arena.deinit();
+
+    var allocator = &arena.allocator;
+    var args_it = os.args();
+
+    // skip args[0]
+
+    _ = args_it.skip(allocator);
+
+    const url = try (args_it.next(allocator) orelse {
+        std.debug.warn("no url provided");
+        return error.InvalidArgs;
+    });
 }
