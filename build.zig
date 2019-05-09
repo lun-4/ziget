b1f43a7aac51046377af6ce91384d91db6141794
Luna <git@l4.pm>
2019-05-09 17:12:08 -0300

    initial commit

---
+++ build.zig
@@ -0,0 +1,16 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("ziget", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.linkSystemLibrary("c");
+
+    const run_cmd = exe.run();
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
+}
