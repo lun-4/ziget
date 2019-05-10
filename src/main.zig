35ea3ae44ec914cce63c4b377917124aeb56e249
Luna <git@l4.pm>
2019-05-09 22:15:52 -0300

    add some socket code

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,20 @@
 const std = @import("std");
 const os = std.os;
 
+const c = @cImport({
+    @cInclude("sys/types.h");
+    @cInclude("sys/socket.h");
+    @cInclude("netinet/in.h");
+    @cInclude("arpa/inet.h");
+    @cInclude("stdio.h");
+});
+
+const ZigetError = error{
+    CreateSockFail,
+    InvalidAddr,
+    ConnectError,
+};
+
 pub fn main() anyerror!void {
     var underlying_allocator = std.heap.c_allocator;
 
@@ -14,17 +28,51 @@ pub fn main() anyerror!void {
 
     _ = args_it.skip();
 
-    const url = try (args_it.next(allocator) orelse {
-        std.debug.warn("no url provided\n");
+    const host = try (args_it.next(allocator) orelse {
+        std.debug.warn("no host provided\n");
         return error.InvalidArgs;
     });
 
-    // TODO: do parsing of url to give it as default for output path
+    const remote_path = try (args_it.next(allocator) orelse {
+        std.debug.warn("no remote path provided\n");
+        return error.InvalidArgs;
+    });
 
-    const path = try (args_it.next(allocator) orelse {
+    const output_path = try (args_it.next(allocator) orelse {
         std.debug.warn("no path provided\n");
         return error.InvalidArgs;
     });
 
-    std.debug.warn("url: {} output path: {}\n", url, path);
+    std.debug.warn("host: {} remote: {} output path: {}\n", host, remote_path, output_path);
+
+    // for some reason c.SOCK_STREAM doesn't work because of typing
+    var sock = c.socket(c.AF_INET, 2, 0);
+    if (sock < 0) {
+        std.debug.warn("failed to create socket\n");
+        return ZigetError.CreateSockFail;
+    }
+
+    std.debug.warn("sock fd: {}\n", sock);
+
+    // make addr struct
+    var addr: c.struct_sockaddr_in = undefined;
+
+    addr.sin_family = c.AF_INET;
+    addr.sin_port = c.htons(80);
+
+    var host_c: [*c]const u8 = @ptrCast([*c]const u8, &host);
+
+    _ = c.printf(c"host_c from c: '%s'\n", host_c);
+
+    if (c.inet_pton(c.AF_INET, host_c, &addr.sin_addr) <= 0) {
+        return ZigetError.InvalidAddr;
+    }
+
+    var addr_c: [*c]const c.struct_sockaddr = @ptrCast([*c]const c.struct_sockaddr, &addr);
+
+    if (c.connect(sock, addr_c, @sizeOf(c.struct_sockaddr_in)) < 0) {
+        return ZigetError.ConnectError;
+    }
+
+    std.debug.warn("owo???\n");
 }
