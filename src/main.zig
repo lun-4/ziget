85405d6a84fd45a0c0f4fdec2a0c5dc459a7636a
Luna <git@l4.pm>
2019-05-12 00:43:03 -0300

    add writing to file

--- src/main.zig
+++ src/main.zig
@@ -39,24 +39,36 @@ pub fn main() anyerror!void {
 
     std.debug.warn("host: {} remote: {} output path: {}\n", host, remote_path, output_path);
 
-    var sockfd = try std.os.posixSocket(std.os.linux.AF_INET, std.os.linux.SOCK_STREAM, 0);
-    std.debug.warn("sock fd: {}\n", sockfd);
+    var sockfd = try os.posixSocket(os.linux.AF_INET, os.linux.SOCK_STREAM, 0);
 
+    // TODO: find a way to call gethostbyname() or some other way to DNS,
+    // prefferably without libc
     var ip4addr = try std.net.parseIp4(host);
+
+    // NOTE: no https support, hopefully by 0.5.0 we have something.
     var addr = std.net.Address.initIp4(ip4addr, 80);
     const const_addr = &addr.os_addr;
-    try std.os.posixConnect(sockfd, const_addr);
+    try os.posixConnect(sockfd, const_addr);
 
     var buffer: [256]u8 = undefined;
     const base_http = "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n";
     var msg = try std.fmt.bufPrint(&buffer, base_http, remote_path, host);
 
-    try std.os.posixWrite(sockfd, msg);
+    try os.posixWrite(sockfd, msg);
 
-    // TODO: read more
     var buf: [1024]u8 = undefined;
-    var read_bytes = try std.os.posixRead(sockfd, &buf);
+    var byte_count: usize = 0;
+    var total_bytes: usize = 0;
 
-    std.debug.warn("read {} bytes\n", read_bytes);
-    std.debug.warn("buf = '{}'\n", buf);
+    var file = try os.File.openWrite(output_path);
+
+    while (true) {
+        byte_count = try std.os.posixRead(sockfd, &buf);
+        if (byte_count == 0) break;
+
+        try file.write(buf);
+        total_bytes += byte_count;
+    }
+
+    std.debug.warn("written {} bytes to file '{}'\n", total_bytes, output_path);
 }
