fd02193968f5307a1a7d3b7b8ef4aabad23fe97d
Luna <git@l4.pm>
2019-06-24 11:51:02 -0300

    update to latest zig

--- src/main.zig
+++ src/main.zig
@@ -10,13 +10,11 @@ const ZigetError = error{
 };
 
 pub fn main() anyerror!void {
-    var underlying_allocator = std.heap.c_allocator;
-
-    var arena = std.heap.ArenaAllocator.init(underlying_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
     defer arena.deinit();
 
     var allocator = &arena.allocator;
-    var args_it = os.args();
+    var args_it = std.process.args();
 
     // skip args[0]
 
@@ -39,7 +37,7 @@ pub fn main() anyerror!void {
 
     std.debug.warn("host: {} remote: {} output path: {}\n", host, remote_path, output_path);
 
-    var sockfd = try os.posixSocket(os.linux.AF_INET, os.linux.SOCK_STREAM, 0);
+    var sockfd = try os.socket(os.linux.AF_INET, os.linux.SOCK_STREAM, 0);
     defer os.close(sockfd);
 
     // TODO: find a way to call gethostbyname() or some other way to DNS,
@@ -48,24 +46,23 @@ pub fn main() anyerror!void {
 
     // NOTE: no https support, hopefully by 0.5.0 we have something.
     var addr = std.net.Address.initIp4(ip4addr, 80);
-    const const_addr = &addr.os_addr;
-    try os.posixConnect(sockfd, const_addr);
+    try os.connect(sockfd, &addr.os_addr, @sizeOf(os.sockaddr));
 
     var buffer: [256]u8 = undefined;
     const base_http = "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n";
     var msg = try std.fmt.bufPrint(&buffer, base_http, remote_path, host);
 
-    try os.posixWrite(sockfd, msg);
+    try os.write(sockfd, msg);
 
     var buf: [1024]u8 = undefined;
     var byte_count: usize = 0;
     var total_bytes: usize = 0;
 
-    var file = try os.File.openWrite(output_path);
+    var file = try std.fs.File.openWrite(output_path);
     defer file.close();
 
     while (true) {
-        byte_count = try std.os.posixRead(sockfd, &buf);
+        byte_count = try std.os.read(sockfd, &buf);
         if (byte_count == 0) break;
 
         try file.write(buf);
